17d28804c267e37a6aa9ae809b08f2cfeb34e5ef
MASTER870-CMD <shashankgowdanb166@gmail.com>
2026-09-25 15:20:36 +0530

    feat: convert app to PWA for APK generation

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   distDir: 'dist',
   eslint: {
     ignoreDuringBuilds: true,
@@ -10,4 +16,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
